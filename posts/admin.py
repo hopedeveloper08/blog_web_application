5d3eee08eb9461f1360577efdb0d89b5e7fe53d6
hopedeveloper08 <hopedeveloper08@gmail.com>
2024-09-18 14:06:40 +0330

    admin panel for post

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug',
+        'is_published',
+        'published_at',
+    )
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'published_at', 'created_at')
+    search_fields = ('title', 'slug')
+    ordering = ('is_published', 'published_at')
+    date_hierarchy = 'published_at'
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
