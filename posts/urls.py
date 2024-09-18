277605841ac6083d95154a057768d280bb9a2802
hopedeveloper08 <hopedeveloper08@gmail.com>
2024-09-18 14:49:18 +0330

    urls for post view

---
+++ posts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>/',
+         views.PostDetailView.as_view(), name='post_detail'),
+]
