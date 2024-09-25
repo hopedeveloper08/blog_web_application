fd7bae060b4ef69bbdab9d9ad7d4bf8403cd1da2
hopedeveloper08 <hopedeveloper08@gmail.com>
2024-09-25 22:04:23 +0330

    add sitemap

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.sitemaps.views import sitemap
+
+from posts.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap,
+}
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('posts.urls', namespace='posts')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),
 ]
